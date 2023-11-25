0bed79c2df0044b0611c9f84e2a7a8e519a6a97b
Prantosh Bepari <bepariprantosh360@gmail.com>
2023-11-25 21:58:18 +0600

    add root route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/user/user.route';
 
@@ -8,7 +8,15 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+const getAController = (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to the API of first project',
+  });
+};
+
 // application routes
+app.use('/', getAController);
 app.use('/api', StudentRoutes);
 
 export default app;
